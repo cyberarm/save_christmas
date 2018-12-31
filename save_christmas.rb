9d8db97594b9474cfeb24ca947d0c3e2fd99c052
Cyberarm <matthewlikesrobots@gmail.com>
2018-12-31 16:46:51 -0600

    Added robot char, refactored for clearer code.

--- save_christmas.rb
+++ save_christmas.rb
@@ -1,73 +1,42 @@
+GameObject = Struct.new(:sprite, :x, :y, :z, :x_velocity, :y_velocity, :jumping)
+
 def init
-  @x = 0
-  @y = 1
-  @z = 0
-#  @x_vel = 0
-  @y_vel = 0
-
-  @sprite = 0
-
-  @jumping = false
+  @player = GameObject.new(20, 0, 1, 0, 0, 0, false)
+  @current_level = 0
 
   @collisions = []
 
   @viewport_x, @viewport_y = 0, 0
   @debug = false
+
   @last_change = milliseconds
   @change_interval = 150
-
   @swapped = false
 end
 
 def draw
   translate(@viewport_x, @viewport_y) do
-    level(0)
-    sprite(@sprite, @x, @y, @z)
+    level(@current_level)
+    sprite(@player.sprite, @player.x, @player.y, @player.z)
 
-    draw_level_boxes(0) if @debug
-    draw_sprite_box(@sprite, @x, @y) if @debug
-
-    rect(16 * 4, 16 * 4, 16, 16, white)
-
-    if @debug
-      @collisions.each do |sprite|
-        edges = colliding_edge(@sprite, @x, @y, sprite.sprite, sprite.x, sprite.y)
-        render_bounding_box(
-          sprite.sprite, @collision_detection.box(sprite.sprite), sprite.x, sprite.y, edges
-        )
-        # p edges
-      end
-    end
+    debug_draw
   end
 
   text(fps.to_s)
 end
 
 def update
- # @collision_detection.clear
+  input_handler
 
-  @x += 1 if button?("right")
-  @x -= 1 if button?("left")
+  @player.x+=@player.x_velocity
+  @player.y+=@player.y_velocity
 
-  #@y += 1 if button?("down")
-  if (button?("x") || button?("up")) && !@jumping
-    @y_vel = -2.5
-    @jumping = true unless @jumping
-  end
+  @viewport_x = width/2 - @player.x
 
-  @y+=@y_vel
-  #@y_vel += 0.05 unless @y_vel >= 0
-
-  if button?("x") && button?("y")
-    @debug = !@debug
-  end
-
-  @viewport_x = width/2 - @x
-
-  if @y < height/4
-    @viewport_y = height/4 - @y
-  elsif @y > height
-    @viewport_y = height/4 - @y
+  if @player.y < height/4
+    @viewport_y = height/4 - @player.y
+  elsif @player.y > height
+    @viewport_y = height/4 - @player.y
   else
     @viewport_y = 0
   end
@@ -76,34 +45,72 @@ def update
     @last_change = milliseconds
     @swapped = !@swapped
 
-    swap(0, 6, 15) unless @swapped
-    swap(0, 15, 6) if @swapped
+    swap(@current_level, 6, 15) unless @swapped
+    swap(@current_level, 15, 6) if @swapped
   end
 
-  @collisions = sprite_vs_level(@sprite, @x, @y, 0)
-  @collisions.delete_if {|s| s.z != @z}
+  collision_handler
+
+  if @player.y > height+16
+    @player.y = 0
+    @player.y_velocity = 0
+  end
+end
+
+def debug_draw
+  if @debug
+    draw_level_boxes(@current_level)
+    draw_sprite_box(@player.sprite, @player.x, @player.y)
+
+    @collisions.each do |sprite|
+      edges = colliding_edge(@player.sprite, @player.x, @player.y, sprite.sprite, sprite.x, sprite.y)
+      render_bounding_box(
+        sprite.sprite, @collision_detection.box(sprite.sprite), sprite.x, sprite.y, edges
+      )
+    end
+  end
+end
+
+def input_handler
+  @player.x += 1 if button?("right")
+  @player.x -= 1 if button?("left")
+
+  if (button?("x") || button?("up")) && !@player.jumping
+    @player.y_velocity = -2.5
+    @player.jumping = true unless @player.jumping
+  end
+
+  if button?("x") && button?("y")
+    @debug = !@debug
+  end
+end
+
+
+def collision_handler
+  @collisions = sprite_vs_level(@player.sprite, @player.x, @player.y, @current_level)
+  @collisions.delete_if {|s| s.z != @player.z}
 
   if @collisions.size > 0
-    my_box = bounding_box(@sprite)
+    my_box = bounding_box(@player.sprite)
     @collisions.each do |sprite|
-      edges = colliding_edge(@sprite, @x, @y, sprite.sprite, sprite.x, sprite.y)
-      # puts "#{sprite.object_id} -> #{edges}"
+      edges = colliding_edge(@player.sprite, @player.x, @player.y, sprite.sprite, sprite.x, sprite.y)
       box = bounding_box(sprite.sprite)
 
-      @y = sprite.y - (my_box.height + 1) if edges[:top]
-      @jumping = false if edges[:top]
-      @y = sprite.y + box.y + box. height + 1 if edges[:bottom]
-      @y_vel = 0 if edges[:bottom]
+      if edges[:top]
+        @player.y = sprite.y - (my_box.height + my_box.y)
+        @player.jumping = false
+        @player.y_velocity = 0
+      end
 
-      @x = (sprite.x + (my_box.x*3) + my_box.width) if edges[:right] && @y + my_box.y + my_box.height > sprite.y + 6
-      @x = (sprite.x - ((my_box.x*2) + my_box.width)) if edges[:left] && @y + my_box.y + my_box.height > sprite.y + 6
+      if edges[:bottom]
+        @player.y = sprite.y + box.y + box.height + 1
+        @player.y_velocity = 0
+      end
+
+      @player.x = (sprite.x - ((my_box.x + 1) + my_box.width)) if edges[:left]  && @player.y + my_box.y + my_box.height > sprite.y + 6
+      @player.x = (sprite.x + (my_box.x +  1) + my_box.width)  if edges[:right] && @player.y + my_box.y + my_box.height > sprite.y + 6
     end
   else
-    @y_vel += 0.10 if @y_vel < 3.1
-  end
-
-  if @y > height+16
-    @y = 0
-    @y_vel = 0
+    @player.y_velocity += 0.10 if @player.y_velocity < 3.1
   end
 end
